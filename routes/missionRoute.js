4ae2cc156ca3c3bdad508e4ec4a84aafa0eef3a0
Apalahdek <144553930+Apalahdek@users.noreply.github.com>
2024-09-04 19:09:08 +0700

    Delete routes directory

--- routes/missionRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const Missions = require("../controller/missions");
-const router = express.Router();
-
-router.get("/", Missions.getMissionList);
-
-module.exports = router;
